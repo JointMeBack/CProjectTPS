deb25fbe67ca5cb6be1a8a9d70587e1713e5efa8
KopBuH <fargate664@gmail.com>
2026-03-03 15:46:54 +0700

    Init commit

---
+++ Source/CProjectTPS/CProjectTPS.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CProjectTPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CProjectTPS, "CProjectTPS" );
+
+DEFINE_LOG_CATEGORY(LogCProjectTPS)
